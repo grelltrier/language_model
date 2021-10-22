c1d5447ae2a780a94b165d5145572420d2ed1a5f
grelltrier <pentamassiv@posteo.de>
2021-10-22 22:00:49 +0200

    Rust edition 2021

--- src/tests.rs
+++ src/tests.rs
@@ -29,6 +29,7 @@ fn cmp(a: Vec<(&str, f32)>, b: Vec<(&str, f32)>) -> bool {
 }
 
 #[test]
+/// Test case D1
 /// Convert the text files to the language model and write it to a
 /// compressed binary file
 fn test_convert_and_load_model() {
@@ -67,97 +68,7 @@ fn test_convert_and_load_model() {
 }
 
 #[test]
-/// Test the language model
-fn test_transitions_and_backoffs() {
-    convert_text_to_cmprssd_bin(true).unwrap();
-    println!("Loading language model from file...");
-    let fname_language_model = "ngrams_test/language_model.bin";
-    let language_model = LanguageModel::read(fname_language_model).unwrap();
-    println!("Done loading");
-    println!();
-
-    let mut predictions;
-    let mut lm_state;
-    let mut correct_prediction;
-    let mut correct_state;
-
-    // Start in initial state
-    lm_state = LMState::default();
-    correct_state = LMState {
-        last_processed_label: 0,
-        ngrams_offset: 0,
-        ngrams_no: usize::MAX,
-        context_len: LMContext::Zero,
-    };
-    assert!(lm_state == correct_state);
-    predictions = language_model.predict(lm_state, 10);
-    correct_prediction = vec![("a", -0.6931472), ("b", -0.6931472)];
-    assert!(cmp(predictions, correct_prediction));
-    println!("Initial state okay");
-    println!();
-
-    // Transition to state 1
-    lm_state = language_model.get_next_state(lm_state, "a");
-    correct_state = LMState {
-        last_processed_label: 0,
-        ngrams_offset: 0,
-        ngrams_no: 1,
-        context_len: LMContext::One,
-    };
-    assert!(lm_state == correct_state);
-    predictions = language_model.predict(lm_state, 10);
-    correct_prediction = vec![("b", -0.40546507), ("a", -1.60943796)];
-    assert!(cmp(predictions, correct_prediction));
-    println!("State 1 okay");
-    println!();
-
-    // Transition to state 3
-    lm_state = language_model.get_next_state(lm_state, "b");
-    correct_state = LMState {
-        last_processed_label: 1,
-        ngrams_offset: 0,
-        ngrams_no: 2,
-        context_len: LMContext::Two,
-    };
-    assert!(lm_state == correct_state);
-    predictions = language_model.predict(lm_state, 10);
-    correct_prediction = vec![("a", -0.6931472), ("b", -0.6931472)];
-    assert!(cmp(predictions, correct_prediction));
-    println!("State 3 okay");
-    println!();
-
-    // Transition to state 5
-    lm_state = language_model.get_next_state(lm_state, "b");
-    correct_state = LMState {
-        last_processed_label: 1,
-        ngrams_offset: 3,
-        ngrams_no: 1,
-        context_len: LMContext::Two,
-    };
-    assert!(lm_state == correct_state);
-    predictions = language_model.predict(lm_state, 10);
-    correct_prediction = vec![("a", 0.0), ("b", -2.01490306)];
-    assert!(cmp(predictions, correct_prediction));
-    println!("State 5 okay");
-    println!();
-
-    // Backoff to state 2
-    lm_state = language_model.backoff(lm_state);
-    correct_state = LMState {
-        last_processed_label: 1,
-        ngrams_offset: 1,
-        ngrams_no: 2,
-        context_len: LMContext::One,
-    };
-    assert!(lm_state == correct_state);
-    predictions = language_model.predict(lm_state, 10);
-    correct_prediction = vec![("a", -0.40546507), ("b", -1.0986123)];
-    assert!(cmp(predictions, correct_prediction));
-    println!("Backoff okay");
-    println!();
-}
-
-#[test]
+/// Test case D2
 /// Test transitioning to the next state (no backoff required)
 fn test_valid_transitions() {
     convert_text_to_cmprssd_bin(true).unwrap();
@@ -270,6 +181,7 @@ fn test_valid_transitions() {
 }
 
 #[test]
+/// Test case D3
 /// Test transitioning to the next state (backoff required)
 fn test_invalid_transitions() {
     convert_text_to_cmprssd_bin(true).unwrap();
@@ -314,7 +226,8 @@ fn test_invalid_transitions() {
 }
 
 #[test]
-/// Test transitioning to the next state (no backoff required)
+/// Test case D4
+/// Backoff to the state associated with the suffix
 fn test_backoff() {
     convert_text_to_cmprssd_bin(true).unwrap();
     println!("Loading language model from file...");
@@ -385,6 +298,98 @@ fn test_backoff() {
     println!("dest   : {:?}", lm_state);
     println!();
     assert!(lm_state == correct_state);
+}
+
+#[test]
+/// Test case D5
+/// Test transitions, backoffs and predictions
+fn test_transitions_and_backoffs() {
+    convert_text_to_cmprssd_bin(true).unwrap();
+    println!("Loading language model from file...");
+    let fname_language_model = "ngrams_test/language_model.bin";
+    let language_model = LanguageModel::read(fname_language_model).unwrap();
+    println!("Done loading");
+    println!();
+
+    let mut predictions;
+    let mut lm_state;
+    let mut correct_prediction;
+    let mut correct_state;
+
+    // Start in initial state
+    lm_state = LMState::default();
+    correct_state = LMState {
+        last_processed_label: 0,
+        ngrams_offset: 0,
+        ngrams_no: usize::MAX,
+        context_len: LMContext::Zero,
+    };
+    assert!(lm_state == correct_state);
+    predictions = language_model.predict(lm_state, 10);
+    correct_prediction = vec![("a", -0.6931472), ("b", -0.6931472)];
+    assert!(cmp(predictions, correct_prediction));
+    println!("Initial state okay");
+    println!();
+
+    // Transition to state 1
+    lm_state = language_model.get_next_state(lm_state, "a");
+    correct_state = LMState {
+        last_processed_label: 0,
+        ngrams_offset: 0,
+        ngrams_no: 1,
+        context_len: LMContext::One,
+    };
+    assert!(lm_state == correct_state);
+    predictions = language_model.predict(lm_state, 10);
+    correct_prediction = vec![("b", -0.40546507), ("a", -1.60943796)];
+    assert!(cmp(predictions, correct_prediction));
+    println!("State 1 okay");
+    println!();
+
+    // Transition to state 3
+    lm_state = language_model.get_next_state(lm_state, "b");
+    correct_state = LMState {
+        last_processed_label: 1,
+        ngrams_offset: 0,
+        ngrams_no: 2,
+        context_len: LMContext::Two,
+    };
+    assert!(lm_state == correct_state);
+    predictions = language_model.predict(lm_state, 10);
+    correct_prediction = vec![("a", -0.6931472), ("b", -0.6931472)];
+    assert!(cmp(predictions, correct_prediction));
+    println!("State 3 okay");
+    println!();
+
+    // Transition to state 5
+    lm_state = language_model.get_next_state(lm_state, "b");
+    correct_state = LMState {
+        last_processed_label: 1,
+        ngrams_offset: 3,
+        ngrams_no: 1,
+        context_len: LMContext::Two,
+    };
+    assert!(lm_state == correct_state);
+    predictions = language_model.predict(lm_state, 10);
+    correct_prediction = vec![("a", 0.0), ("b", -2.01490306)];
+    assert!(cmp(predictions, correct_prediction));
+    println!("State 5 okay");
+    println!();
+
+    // Backoff to state 2
+    lm_state = language_model.backoff(lm_state);
+    correct_state = LMState {
+        last_processed_label: 1,
+        ngrams_offset: 1,
+        ngrams_no: 2,
+        context_len: LMContext::One,
+    };
+    assert!(lm_state == correct_state);
+    predictions = language_model.predict(lm_state, 10);
+    correct_prediction = vec![("a", -0.40546507), ("b", -1.0986123)];
+    assert!(cmp(predictions, correct_prediction));
+    println!("Backoff okay");
+    println!();
 }
 
 fn get_test_state_no(state_no: usize) -> LMState {
